b5d950305ae5a3d1158858187884e3beda3bf451
shrijith-shetty <151986938+shrijith-shetty@users.noreply.github.com>
2025-12-02 13:54:34 +0530

    Add mTextStyle function for customizable text style

---
+++ 6_extended.dartimport 'package:flutter/material.dart';  TextStyle mTextStyle( {   Color textColor = Colors.green, }     ){   return TextStyle(     fontSize: 11,     color: textColor,     /6_extended.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+TextStyle mTextStyle(
+{
+  Color textColor = Colors.green,
+}
+    ){
+  return TextStyle(
+    fontSize: 11,
+    color: textColor,
+    // fontWeight: FontWeight,
+  );
+}
+
+
+TextStyle mTextStyle21(){
+  return TextStyle(
+    fontSize: 11,
+  );
+}
