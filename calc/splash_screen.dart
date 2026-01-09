aec200e3cfc8514e4dca404268d9b51d1ca0a6ed
shrijith-shetty <151986938+shrijith-shetty@users.noreply.github.com>
2026-01-09 17:54:01 +0530

    Implement splash screen with navigation

---
+++ calc/splash_screen.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:calc/main.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SpashScreen extends StatefulWidget
+{
+
+    @override
+    State<SpashScreen> createState() => _SpashScreenState();
+}
+
+class _SpashScreenState extends State<SpashScreen>
+{
+    @override
+    void initState()
+    {
+        super.initState();
+        Timer(Duration(seconds: 3), ()
+            {
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => MyHomePage(title: "Flutter")));
+            });
+    }
+    @override
+    Widget build(BuildContext context) 
+    {
+        // TODO: implement build
+        return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+                Icon(Icons.calculate, color: Colors.orange, size: 200),
+                // SizedBox(height: 0,),
+                Text("Calculator", style: TextStyle(fontSize: 42, color: Colors.orange))
+            ]
+        );
+    }
+}
